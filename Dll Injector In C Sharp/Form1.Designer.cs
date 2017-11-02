3ace413e33c4ce176af8adbcf106a1e84a7fd960
Sheraz Ibrahim <sherazibrahim786@gmail.com>
2017-11-02 10:43:35 +0500

    Dll Injector

---
+++ Dll Injector In C Sharp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Dll_Injector_In_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Name : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 50);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(362, 63);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Inject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 149);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Dll Injector In C Sharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Dll Injector In C Sharp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+namespace Dll_Injector_In_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int AccessType, bool InheritHandle, int ProcessId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string IpModuleName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr ModuleHandle, string processname);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hprocess, IntPtr ipaddress, int dwsize, uint flallocationtype, uint flprotect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hprocess, IntPtr IpBaseAddress, byte[] IpBuffer, int nsize, out UIntPtr IpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hprocess, IntPtr IpThreadAttributes, uint dwstakesize, IntPtr IpStartAddress, IntPtr IpParameter, uint dwCreationFlags, IntPtr IpThreadId);
+
+
+
+        const int PROCESS_CREATETHREAD = 0X002;
+        const int PROCESS_QUERY_INFORMATION = 0X0400;
+        const int PROCESS_VM_OPERATIONS = 0X0008;
+        const int PROCESS_VM_WRITE = 0X0020;
+        const int PROCESS_VM_READ = 0X0010;
+
+        const uint MEM_COMMIT = 0X00001000;
+        const uint MEM_RESERVE = 0X0002000;
+        const uint PAGE_READWRITE = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox2.Text = openFileDialog1.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void inject(String Processname)
+        {
+            try
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    string path;
+                    path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\SMP.dll";
+                    File.WriteAllBytes(path, Properties.Resources.ShowMsgInProcess);
+                    IntPtr LoadLibraryA = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+                    Process processm = Process.GetProcessesByName(Processname)[0];
+                    IntPtr ProcessHandle = OpenProcess(PROCESS_CREATETHREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATIONS | PROCESS_VM_WRITE | PROCESS_VM_READ, false, processm.Id);
+                    byte[] bytes = Encoding.ASCII.GetBytes(path);
+                    IntPtr allocateMemAddress = VirtualAllocEx(ProcessHandle, IntPtr.Zero, bytes.Length + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+                    UIntPtr bytesWritten;
+                    WriteProcessMemory(ProcessHandle, allocateMemAddress, bytes, bytes.Length, out bytesWritten);
+                    CreateRemoteThread(ProcessHandle, IntPtr.Zero, 0, LoadLibraryA, allocateMemAddress, 0, IntPtr.Zero);
+                    MessageBox.Show("Dll Injected.....");
+                }
+                else
+                {
+                    MessageBox.Show("Fill all Boxes........");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+            private void button2_Click(object sender, EventArgs e)
+        {
+          
+            inject(textBox1.Text);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text.Length > 0)
+                {
+                    string path;
+                    path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\Message.txt";
+                    File.WriteAllText(path, textBox2.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
